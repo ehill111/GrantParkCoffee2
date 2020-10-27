8e99a70c3f8c04b07cfa6f1d320e0fec9726e2a1
Eric <ehill111@comcast.net>
2020-10-27 14:34:03 -0400

    Added session logic in ShoppingCart to provide session capabilities.

--- Models/ShoppingCart.cs
+++ Models/ShoppingCart.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -9,6 +11,12 @@ namespace GrantParkCoffeeShop2.Models
 {
     public class ShoppingCart
     {
+        private readonly AppDbContext _appDbContext;
+        private ShoppingCart(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
         [Key]
         public string ShoppingCartId { get; set; }
         public List<ShoppingCartItem> ShoppingCartItems { get; set; }
@@ -22,7 +30,20 @@ namespace GrantParkCoffeeShop2.Models
         public int ProductId { get; set; }
         public Product Product { get; set; }
 
+        public static ShoppingCart GetCart(IServiceProvider services)
+        {
+            ISession session = services.GetRequiredService<IHttpContextAccessor>()?
+                .HttpContext.Session;
 
+            var context = services.GetService<AppDbContext>();
+            string cartId = session.GetString("CartId") ?? Guid.NewGuid().ToString();
+
+            session.SetString("CartId", cartId);
+
+            return new ShoppingCart(context) { ShoppingCartId = cartId };
+
+                 
+        }
        
     }
 }
