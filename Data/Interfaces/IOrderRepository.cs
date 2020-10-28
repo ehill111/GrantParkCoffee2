d9b7a996409e3b12fbbd2c5b490fe9f5041a0afc
Eric <ehill111@comcast.net>
2020-10-28 19:53:17 -0400

    Added Interface folder to Database. Added IOrderRepository.

---
+++ Data/Interfaces/IOrderRepository.cs
@@ -0,0 +1,13 @@
+﻿using GrantParkCoffeeShop2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrantParkCoffeeShop2.Data.Interfaces
+{
+    public interface IOrderRepository
+    {
+        void CreateOrder(Order order);
+    }
+}
