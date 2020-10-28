36cfc22f4dd238082f8541da8e597d8c7b9fe7df
Eric <ehill111@comcast.net>
2020-10-28 19:59:25 -0400

    Added OrderRepository.

---
+++ Data/Repositories/OrderRepository.cs
@@ -0,0 +1,17 @@
+﻿using GrantParkCoffeeShop2.Data.Interfaces;
+using GrantParkCoffeeShop2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrantParkCoffeeShop2.Data.Repositories
+{
+    public class OrderRepository : IOrderRepository
+    {
+        public void CreateOrder(Order order)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
