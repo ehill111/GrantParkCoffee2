0ee1160f80bc42cfd71091503002a7799d25a37a
Eric <ehill111@comcast.net>
2020-10-28 20:06:00 -0400

    Injected ApplicationDbContext method into OrderRespository.

--- Data/Repositories/OrderRepository.cs
+++ Data/Repositories/OrderRepository.cs
@@ -9,6 +9,15 @@ namespace GrantParkCoffeeShop2.Data.Repositories
 {
     public class OrderRepository : IOrderRepository
     {
+        private readonly ApplicationDbContext _appDbContext;
+        private readonly ShoppingCart _shoppingCart;
+
+        public OrderRepository(ApplicationDbContext appDbContext, ShoppingCart shoppingCart)
+        {
+            _appDbContext = appDbContext;
+            _shoppingCart = shoppingCart;
+        }
+
         public void CreateOrder(Order order)
         {
             throw new NotImplementedException();
