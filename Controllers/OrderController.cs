aa4b8c14604b1253cf82a3af5699ca6cda59bb5f
Eric <ehill111@comcast.net>
2020-10-28 20:46:08 -0400

    Injected IOrderRepository.

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -2,12 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GrantParkCoffeeShop2.Data.Interfaces;
+using GrantParkCoffeeShop2.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GrantParkCoffeeShop2.Controllers
 {
     public class OrderController : Controller
     {
+        private readonly IOrderRepository _orderRepository;
+        private readonly ShoppingCart _shoppingCart;
+
+        public OrderController(IOrderRepository orderRepository, ShoppingCart shoppingCart)
+        {
+            _orderRepository = orderRepository;
+            _shoppingCart = shoppingCart;
+        }
+
         public IActionResult Index()
         {
             return View();
