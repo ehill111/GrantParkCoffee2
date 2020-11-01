9d2feac6c0a9928a672e69daa5f2d3423ab333a3
Eric <ehill111@comcast.net>
2020-11-01 18:50:08 -0500

    Added view page for RoomReservation.

---
+++ Data/Entities/RoomReservation.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace GrantParkCoffeeShop2.Data.Entities
+{
+    public class RoomReservation : EntityBase
+    {
+        public DateTime Date { get; set; }
+        public Customer Customer { get; set; }
+    }
+}
