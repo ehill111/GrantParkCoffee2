289e37c62857c76e1725e21e5fe3a62e077b81e8
Eric <ehill111@comcast.net>
2020-11-03 12:04:11 -0500

    Added html body tag to all view pages for css purposes.

--- Data/Entities/Product.cs
+++ Data/Entities/Product.cs
@@ -1,14 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace GrantParkCoffeeShop2.Data.Entities
 {
     public class Product : EntityBase
     {
         public string Name { get; set; }
         public string Description { get; set; }
+        [Display(Name = "Unit Price")]
         public double UnitPrice { get; set; }
         public int Quantity { get; set; }
         public bool Featured { get; set; }
         public string Image { get; set; }
         public double Discount { get; set; }
+        [Display(Name = "Reward Eligible")]
         public bool RewardEligible { get; set; }
         public Category Category { get; set; }
     }
